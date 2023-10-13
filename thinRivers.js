893d032859f6d6c274f1fae91c616ccbc8bdd78a
ZZWILLIAMXX <49970753+ZZWILLIAMXXTrue@users.noreply.github.com>
2023-10-12 21:04:46 -0700

    Add ThinRivers button

--- thinRivers.js
+++ thinRivers.js
@@ -1,4 +1,21 @@
+// You can change numbers to increase width or widdening of the river
 pack.rivers.forEach(r => r.widthFactor=0.0)
 pack.rivers.forEach(r => r.sourceWidth=0.5)
 
-// You can change the numbers at the end to make rivers thinner, wider, or shrink.
+// Button to make life easier
+var buttonThinRivers = document.createElement('button');
+buttonThinRivers.innerHTML = 'Thin Rivers';
+buttonThinRivers.style.position = 'fixed';
+buttonThinRivers.style.top = '10px';
+buttonThinRivers.style.right = '10px';
+buttonThinRivers.style.zIndex = '9999';
+
+document.body.appendChild(buttonThinRivers);
+
+buttonThinRivers.addEventListener('click', function() {
+  pack.rivers.forEach(r => {
+    // Here are the numbers you can change
+    r.widthFactor = 0.5;
+    r.sourceWidth = 0.5;
+  });
+});
