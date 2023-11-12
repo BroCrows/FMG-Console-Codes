f35ecf4d3166df008d2f4265b2a92acae83fcb0b
ZZWILLIAMXX <49970753+ZZWILLIAMXXTrue@users.noreply.github.com>
2023-11-11 22:45:48 -0600

    Create fillColorCharacteristics.js

---
+++ Burgs/fillColorCharacteristics.js
@@ -0,0 +1,18 @@
+pack.burgs.forEach( b => {
+
+if (!b.i) return;
+// Replace citadel with desired desired characterisitic.
+// Options are capital, port, citadel, walls, plaza(trade center), temple(religious center), shanty town
+if (b.placeholder) { =
+
+let x = burgIcons.select("#burg" + b.i);
+
+if (x) {
+// Replace color here. Default is blue
+x.attr("fill", "blue");
+
+}
+
+}
+
+});
