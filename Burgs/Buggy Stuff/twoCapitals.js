35429d7ebfc799c4f1d60aee20522c09e7db2e2f
ZZWILLIAMXX <49970753+ZZWILLIAMXXTrue@users.noreply.github.com>
2023-11-11 22:50:48 -0600

    Create twoCapitals.js

---
+++ Burgs/Buggy Stuff/twoCapitals.js
@@ -0,0 +1,6 @@
+// WARNING WARNING WARNING
+// THIS IS NOT A GOOD IDEA. SETTING THE BURG TO THE STYLE OF CITIES IS 10X BETTER.
+
+// Replace placeholder with burg's name
+pack.burgs.find(x=>x.name=="placeholder").capital=true; 
+BurgsAndStates.drawBurgs();
