a4a9d2ea54d937091f6905241c5a19b79e3e9ab5
ZZWILLIAMXX <49970753+ZZWILLIAMXXTrue@users.noreply.github.com>
2023-11-12 11:32:19 -0600

    Update fixHighPopulation.js

--- fixHighPopulation.js
+++ fixHighPopulation.js
@@ -1,11 +1,11 @@
+const avg = pack.cells.pop.reduce((sum,value) => sum + value, 0) / pack.cells.pop.length;
+// You can modulate the "2" to set your preferred threshold
 pack.cells.pop = pack.cells.pop.map(function (entry) {
-  if (entry > 10) {
-   
-    return Math.random() * 2 + 5.5;;
+  if (entry > (avg*2)) {
+// You can modulate the "0.5" and "2" parameters to adjust your preferred randomness   
+    return Math.random() * (avg * 0.5) + (avg * 2);;
   } else {
 
     return entry;
   }
-});
-
-console.log(pack.cells.pop);
+});;
