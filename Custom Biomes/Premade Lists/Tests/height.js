bfff8704b1b1d4d18abfe2c4e6304a59468ddcb3
BroCrow <49970753+BroCrows@users.noreply.github.com>
2024-07-03 20:40:16 -0500

    Create height.js

---
+++ Custom Biomes/Premade Lists/Tests/height.js
@@ -0,0 +1,6 @@
+custom_biome_list = [
+  ['Plains', '#00ff00', 100, 20, 45, -128, 50, 0, 100000],
+  ['Hills', '#ffff00', 67, 46, 65, -128, 50, 0, 100000],
+  ['Mountains', '#ff0000', 33, 66, 100, -128, 50, 0, 100000]
+];
+generate_custom_biome_list()
